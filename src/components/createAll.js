19f0efec960636e84d3394f13143485462e0a3f9
PapoyDev <gru.papoy.dev@gmail.com>
2015-08-07 14:06:14 +0300

    Move stuff around, finish removing provide() and <Connector>

--- src/components/createAll.js
+++ src/components/createAll.js
@@ -1,17 +1,9 @@
 import createProvider from './createProvider';
-import createProvideDecorator from './createProvideDecorator';
-
-import createConnector from './createConnector';
-import createConnectDecorator from './createConnectDecorator';
+import createConnect from './createConnect';
 
 export default function createAll(React) {
-  // Wrapper components
   const Provider = createProvider(React);
-  const Connector = createConnector(React);
+  const connect = createConnect(React);
 
-  // Higher-order components (decorators)
-  const provide = createProvideDecorator(React, Provider);
-  const connect = createConnectDecorator(React, Connector);
-
-  return { Provider, Connector, provide, connect };
+  return { Provider, connect };
 }
