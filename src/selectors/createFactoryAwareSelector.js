bb08d00c6fe489a4e8014d055df8ee1d8413e3df
PapoyDev <gru.papoy.dev@gmail.com>
2016-07-02 17:54:43 -0400

    Add guard for the case of a component causing its siblings to unsubscribe in the middle of the notification loop.

---
+++ src/selectors/createFactoryAwareSelector.js
@@ -0,0 +1,62 @@
+import memoizeProps from '../utils/memoizeProps'
+
+// factory detection. if the first result of mapToProps is a function, use that as the
+// true mapToProps
+export function createMapOrMapFactoryProxy(mapToProps) {
+  let map = undefined
+  function firstRun(storePart, props) {
+    const result = mapToProps(storePart, props)
+    if (typeof result === 'function') {
+      map = result
+      return map(storePart, props)
+    } else {
+      map = mapToProps
+      return result
+    }
+  }
+
+  function proxy(storePart, props) {
+    return (map || firstRun)(storePart, props)
+  }
+  proxy.dependsOnProps = function dependsOnProps() {
+    return (map || mapToProps).length !== 1
+  }
+  return proxy
+}
+
+export function createImpureFactoryAwareSelector(getStorePart, mapToProps) {
+  const map = createMapOrMapFactoryProxy(mapToProps)
+
+  return function impureFactoryAwareSelector(state, props, dispatch) {
+    return map(
+      getStorePart(state, props, dispatch),
+      props
+    )
+  }
+}
+
+export function createPureFactoryAwareSelector(getStorePart, mapToProps) {
+  const map = createMapOrMapFactoryProxy(mapToProps)
+  const memoizeMapResult = memoizeProps()
+  const noProps = {}
+  let lastStorePart = undefined
+  let lastProps = undefined
+  let result = undefined
+
+  return function pureFactoryAwareSelector(state, props, dispatch) {
+    const nextStorePart = getStorePart(state, props, dispatch)
+    const nextProps = map.dependsOnProps() ? props : noProps
+
+    if (lastStorePart !== nextStorePart || lastProps !== nextProps) {
+      result = memoizeMapResult(map(nextStorePart, nextProps))
+      lastStorePart = nextStorePart
+      lastProps = nextProps
+    }
+    return result
+  }
+}
+
+export default function createFactoryAwareSelector(pure, getStorePart, mapToProps) {
+  const create = pure ? createPureFactoryAwareSelector : createImpureFactoryAwareSelector
+  return create(getStorePart, mapToProps)
+}
